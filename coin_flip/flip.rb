dd5460868cb784c7cec49505fb45a45f39423fbe
Jackson Singleton <jsingleton12321@yahoo.com>
2022-12-22 16:02:21 -0600

    refactor

--- coin_flip/flip.rb
+++ coin_flip/flip.rb
@@ -1,4 +1,4 @@
-TOTAL_LENGTH = 24
+TOTAL_LENGTH = 64
 
 bits = TOTAL_LENGTH.times.map { rand(2) }.join
 PULL_AMT = bits.count('0')
@@ -18,9 +18,10 @@ puts "BEFORE FLIP"
 puts "Group 1: #{bits}   => #{bits.count('0')} zeroes\nGroup 2: #{bits_2}  => #{bits_2.count('0')} zeroes"
 flipped = ""
 
-bits_2.each_char do |c|
-  c == "1" ? flipped += "0" : flipped += "1"
+bits_2.each_char.with_index do |c, i|
+  c == "1" ? bits_2[i] = "0" : bits_2[i] = "1"
 end
 
 puts "AFTER FLIP"
-puts "Group 1: #{bits} => #{bits.count('0')} zeroes\nGroup 2: #{flipped} => #{flipped.count('0')} zeroes"
+puts "Group 1: #{bits}   => #{bits.count('0')} zeroes\nGroup 2: #{bits_2}  => #{bits_2.count('0')} zeroes"
+#puts "Group 1: #{bits} => #{bits.count('0')} zeroes\nGroup 2: #{flipped} => #{flipped.count('0')} zeroes"
